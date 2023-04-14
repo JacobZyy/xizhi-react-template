44728782e388aadeaacec579593ad71dabdc2be8
jacobzha <zhayang@xizhi-ai.com>
2023-04-14 13:40:55 +0800

    feat: userType and logout

--- src/components/UserAvtar/index.tsx
+++ src/components/UserAvtar/index.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { useNavigate } from "react-router";
 import { Avatar, Button, Dropdown, MenuProps, Space } from "antd";
 
 import {
@@ -8,15 +9,19 @@ import {
 } from "@ant-design/icons";
 
 import avataalogo from "@/assets/logo/avatar.png";
+import { storage } from "@/utils/storage";
 
 import styles from "./index.module.scss";
 
 const UserAvtar: React.FC = () => {
+  const navigate = useNavigate();
   const handleChangePwd = () => {
     console.log("修改密码");
   };
 
   const handleLogout = () => {
+    storage.clear();
+    navigate("/login", { replace: true });
     console.log("退出登录");
   };
 
